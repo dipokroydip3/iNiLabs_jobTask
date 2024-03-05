da1d6d967d20c8e577ac402f812653638f051611
Dipak Roy <dipokroydip3@gmail.com>
2024-03-05 05:30:27 -0800

    install tailwindcss for styling

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+
+
+const Navbar = () => {
+      return (
+            <div>
+                  <div className="flex">
+                        <h1>miro</h1>
+                        <ul>
+                              <li>Product</li>
+                              <li>Solutions</li>
+                              <li>Resources</li>
+                              <li>Enterprise</li>
+                              <li>Pricing</li>
+                        </ul>
+                        <div>
+                              <ul>
+                                    <li>EN</li>
+                                    <li>Contact</li>
+                                    <li>Sales</li>
+                              </ul>
+                              <button>Sign up free</button>
+                        </div>
+                  </div>
+            </div>
+      );
+};
+
+export default Navbar;
