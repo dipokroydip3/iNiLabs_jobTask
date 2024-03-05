f8f1c53a3dd652edeeaa2a326d1a06c6609cf982
Dipak Roy <dipokroydip3@gmail.com>
2024-03-05 08:30:10 -0800

    feature section design almoste complete

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -2,8 +2,22 @@
 
 const Feature = () => {
       return (
-            <div>
-                  
+            <div className="w-[1140px] px-5  m-auto">
+                  <h1 className="text-4xl font-semibold text-center py-10">Collaborate without <br /> constraints</h1>
+                  <div className="grid grid-cols-3 py-10 gap-16">
+                        <div>
+                              <h3 className="text-2xl font-semibold">Free forever</h3>
+                              <p className="py-2">Our free plan gives you unlimited team member, 3 boards, and 300+ expert-made templates. signing up with your work email lets you bring in your team faster. See our <span className="text-blue-600">pricing plans</span> for more features.</p>
+                        </div>
+                        <div>
+                              <h3  className="text-2xl font-semibold">Easy integrations</h3>
+                              <p className="py-2">Miro has 100+ powerful integrations with tools you already use like G Suite, Slack, and Jira, so your workflow is seamless. View the full list in our <span className="text-blue-600">Marketplace</span></p>
+                        </div>
+                        <div>
+                              <h3  className="text-2xl font-semibold">Security first</h3>
+                              <p className="py-2">We treat your data like you would - with the utmost care. We follow industry-leading security standards and give you tools to protect intellectual property. Learn more at our<span className="text-blue-600">Trust Center</span></p>
+                        </div>
+                  </div>
             </div>
       );
 };
