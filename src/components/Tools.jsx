d06f5489272894150d5c03ee6b1a28f97a772d12
Dipak Roy <dipokroydip3@gmail.com>
2024-03-05 09:53:32 -0800

    tools section design complete

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -1,9 +1,17 @@
-
+import { FaArrowRight } from "react-icons/fa6";
 
 const Tools = () => {
       return (
             <div>
-                  
+                  <div className="grid grid-cols-2 gap-20 items-center w-[1140px] justify-between m-auto">
+                        <img className="" src="https://i.ibb.co/Nxq502v/6a9b1a555d15b5769a5ec2b1870f5af7-removebg-preview.png" alt="" />
+                        <div className="">
+                              <h2 className="text-5xl font-bold">Connect<br />your tools, <br />close your tabs</h2>
+                              <p className="py-5 pr-40">Whether you want to edit your Google Docs, resolve Jira issues, or collaborate over Zoom, Miro has 100
+                                    + integrations with tools you already use and love.</p>
+                              <button className="text-blue-600 gap-3 underline flex items-center">Learn more <FaArrowRight className="mt-1" /> </button>
+                        </div>
+                  </div>
             </div>
       );
 };
--- src/components/WorkTogether.jsx
+++ src/components/WorkTogether.jsx
@@ -2,9 +2,9 @@ import { FaArrowRight } from "react-icons/fa6";
 
 const WorkTogether = () => {
       return (
-            <div className="grid grid-cols-2 w-[1140px] py-16  m-auto">
-                  <div className="pt-6">
-                        <h2 className="text-4xl font-bold">Work together wherever <br /> you work</h2>
+            <div className="grid grid-cols-2 items-center w-[1140px] py-16  m-auto">
+                  <div className="">
+                        <h2 className="text-4xl font-bold">Work together <br /> wherever you work</h2>
                         <p className="py-5 pr-40">In the office , remote, or a mix of the two, with Miro, your team cat connect, collaborate, and co-create in one space no matter where you are.</p>
                         <button className="text-blue-600 gap-3 underline flex items-center">Learn more <FaArrowRight className="mt-1"/> </button>
                   </div>
