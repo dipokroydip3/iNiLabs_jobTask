091eae378e15ebad2f4ac731a1de8dfb8c946d09
Dipak Roy <dipokroydip3@gmail.com>
2024-03-05 07:25:36 -0800

    trusted section design almost complete

--- src/components/Trusted.jsx
+++ src/components/Trusted.jsx
@@ -1,9 +1,18 @@
-
+import { SiWalmart } from "react-icons/si";
+import { SiCisco } from "react-icons/si";
+import { SiVolvo } from "react-icons/si";
 
 const Trusted = () => {
       return (
             <div>
-                  
+                  <p className="text-center py-5">Trusted by 45m+ users</p>
+                  <div className="flex px-12 flex-wrap justify-between list-none w-[1140px] m-auto items-center">
+                        <SiWalmart className=" text-9xl" />
+                        <SiCisco className=" text-6xl" />
+                        <SiVolvo className=" text-6xl" />
+                        <li className="font-bold text-2xl">Deloitte.</li>
+                        <li className="font-bold text-2xl">okta</li>
+                  </div>
             </div>
       );
 };
